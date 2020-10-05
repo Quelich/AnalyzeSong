ba104f8f796d9f1ffaf1135919f6238f6414c5b7
Emre KILIC <emre1kilic@outlook.com>
2020-10-05 15:12:00 +0300

    Added IOReader and IOWriter

--- IOReader/Program.cs
+++ IOReader/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
 
-namespace IOReader
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace IOReader {
+    class Program {
+        //TODO add AI framework to detect the curses
+        static void Main (string[] args) {
+        }
+        public void IOReader () {
+            //! You need to key in the direction of the file yet
+            string Text = File.ReadAllText (@"D:\GitRepos\FileIOc\IOReader\SampleText.txt");
+            Console.WriteLine ($"{Text}");
+            Console.ReadKey ();
+        }
+        public void IOWriter () {
+            string[] lines = { "Hello", "This is Emre", "Just having fun with it" };
+            //! You need to key in the direction of the file yet
+            File.WriteAllLines (@"D:\GitRepos\FileIOc\IOReader\WrittenText.txt", lines);
         }
     }
+
 }
