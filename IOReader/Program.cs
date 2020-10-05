04991c7afa59af5c7f4bcbab5647ee4f656f764a
Emre KILIC <emre1kilic@outlook.com>
2020-10-05 15:32:07 +0300

    Added GotInput

--- IOReader/Program.cs
+++ IOReader/Program.cs
@@ -2,21 +2,37 @@
 using System.IO;
 
 namespace IOReader {
-    class Program {
+    public class Program {
         //TODO add AI framework to detect the curses
-        static void Main (string[] args) {
+        public static void Main (string[] args) {
+
         }
-        public void IOReader () {
+        /// <summary>
+        /// Simple .txt reader
+        /// </summary>
+        public static void IOReader () {
             //! You need to key in the direction of the file yet
             string Text = File.ReadAllText (@"D:\GitRepos\FileIOc\IOReader\SampleText.txt");
             Console.WriteLine ($"{Text}");
             Console.ReadKey ();
         }
-        public void IOWriter () {
+        /// <summary>
+        /// Simple .txt data writer 
+        /// </summary>
+        public static void IOWriter () {
             string[] lines = { "Hello", "This is Emre", "Just having fun with it" };
             //! You need to key in the direction of the file yet
             File.WriteAllLines (@"D:\GitRepos\FileIOc\IOReader\WrittenText.txt", lines);
         }
+        /// <summary>
+        /// Gets input from the user wanting to create a text file
+        /// </summary>
+        public static void GotInput () {
+            Console.WriteLine ("Type the file name...");
+            string fileName = Console.ReadLine ();
+            Console.WriteLine ("Type the content you want");
+            string input = Console.ReadLine ();
+            File.WriteAllText (@"D:\GitRepos\FileIOc\IOReader\" + fileName + ".txt", input);
+        }
     }
-
 }
